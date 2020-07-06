49a348b42943bde7803ac5cc9934a1ccd9c1468a
Isaac Corbrey <icorbrey@apterainc.com>
2020-07-06 13:10:08 -0400

    Lift logic out of PromptLine

--- src/components/PromptLine.tsx
+++ src/components/PromptLine.tsx
@@ -1,55 +1,24 @@
-import React, { KeyboardEvent, useState } from 'react'
+import React, { KeyboardEvent } from 'react'
 import InputRef from '../types/InputRef'
 import ListenerCollection from '../types/ListenerCollection'
-import { DisplayHistoryRecorder } from '../types/DisplayHistory'
-import { InputHistory, InputHistoryRecorder } from '../types/InputHistory'
-
 import Prompt from './Prompt'
 
 interface PromptLineProps
 {
+	input: InputRef
 	success?: boolean
-	inputRef: InputRef
 	currentDir?: string
-	inputHistory: InputHistory
-	addToInputHistory: InputHistoryRecorder
-	addToDisplayHistory: DisplayHistoryRecorder
+	keyDownListeners: ListenerCollection
 }
 
-export default ({ success = true, inputRef, currentDir = '~', inputHistory, addToInputHistory, addToDisplayHistory }: PromptLineProps) =>
+export default ({ keyDownListeners, success = true, input, currentDir = '~' }: PromptLineProps) =>
 {
-	const [inputHistoryIndex, setInputHistoryIndex] = useState(-1)
-
-	const captureInput = (event: KeyboardEvent) =>
+	const onKeyDown = (event: KeyboardEvent) =>
 	{
-		const listeners: ListenerCollection = {
-			'Enter': () =>
-			{
-				const value = getInputValue(inputRef)
-				addToDisplayHistory(generatePromptEntry(success, currentDir, value))
-				if (value && doesNotMatchPreviousCommand(value, inputHistory))
-					addToInputHistory(value)
-				setInputValue(inputRef, '')
-				setInputHistoryIndex(-1)
-			},
-			'ArrowUp': () =>
-			{
-				const value = getPreviousEntry(inputHistoryIndex, inputHistory)
-				setInputHistoryIndex(value)
-				setInputValue(inputRef, inputHistory[value])
-			},
-			'ArrowDown': () =>
-			{
-				const value = getNextEntry(inputHistoryIndex)
-				setInputHistoryIndex(value)
-				setInputValue(inputRef, inputHistory[value])
-			}
-		}
-
-		if (listeners[event.key])
+		if (keyDownListeners[event.key])
 		{
 			event.preventDefault()
-			listeners[event.key]()
+			keyDownListeners[event.key]()
 		}
 
 		scrollToBottom()
@@ -61,11 +30,12 @@ export default ({ success = true, inputRef, currentDir = '~', inputHistory, addT
 				success,
 				dir: currentDir,
 			} } />
-			<input
-				autoFocus
-				ref={ inputRef }
-				className='prompt-input'
-				onKeyDown={ captureInput } />
+			<input { ...{
+				onKeyDown,
+				ref: input,
+				autoFocus: true,
+				className: 'prompt-input',
+			} } />
 		</span>
 	)
 }
@@ -74,27 +44,3 @@ const scrollToBottom = () => window.scrollTo(0, getScrollHeight())
 
 const getScrollHeight = () => document.body.scrollHeight
 	|| document.documentElement.scrollHeight
-
-const getInputValue = (inputRef: InputRef): string | undefined =>
-	inputRef?.current?.value
-
-const setInputValue = (inputRef: InputRef, value: string | undefined) =>
-{
-	if (inputRef && inputRef.current)
-		inputRef.current.value = value || ''
-}
-
-const generatePromptEntry = (success: boolean, dir: string, value: string | undefined) => (
-	<span>
-		<Prompt { ...{ success, dir } } />{ value }
-	</span>
-)
-
-const doesNotMatchPreviousCommand = (value: string, inputHistory: InputHistory) =>
-	value !== inputHistory[0]
-
-const getNextEntry = (inputHistoryIndex: number) =>
-	Math.max(inputHistoryIndex - 1, -1)
-
-const getPreviousEntry = (inputHistoryIndex: number, inputHistory: InputHistory) =>
-	Math.min(inputHistoryIndex + 1, inputHistory.length - 1)
--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -6,12 +6,15 @@ import Welcome from './Welcome'
 import PromptLine from './PromptLine'
 import TerminalWindow from './TerminalWindow'
 import InputRef from '../types/InputRef'
+import Prompt from './Prompt'
+import ListenerCollection from '../types/ListenerCollection'
 
 export default () =>
 {
 	const [success] = useState(true)
 	const [currentDir] = useState('~')
-	const [inputRef] = useState(createRef<HTMLInputElement>())
+	const [input] = useState(createRef<HTMLInputElement>())
+	const [inputHistoryIndex, setInputHistoryIndex] = useState(-1)
 	const [inputHistory, setInputHistory] = useState<InputHistory>([])
 	const [displayHistory, setDisplayHistory] = useState<DisplayHistory>([<Welcome />])
 
@@ -21,26 +24,68 @@ export default () =>
 	const addToDisplayHistory = (entry: DisplayHistoryEntry) =>
 		setDisplayHistory([...displayHistory, entry])
 
+	const offsetInputHistoryIndex = (index: number) =>
+		Math.min(Math.max(inputHistoryIndex + index, -1), inputHistory.length - 1)
+
+	const addValueToInputHistory = (value: string | undefined) =>
+	{
+		if (value && value !== inputHistory[0])
+			addToInputHistory(value)
+	}
+
+	const addPromptToDisplayHistory = (value: string | undefined) =>
+	{
+		addToDisplayHistory(
+			<span>
+				<Prompt { ...{
+					success,
+					dir: currentDir
+				} } />{ value }
+			</span>
+		)
+	}
+
 	const displayEntries = displayHistory.map((entry, key) => (
-		<p { ...{ key } }>
+		<span { ...{ key } }>
 			{ entry }
 			<br />
-		</p>
+		</span>
 	))
 
-	const onTerminalMouseUp = () =>
-		isTextSelected() || focusInputRef(inputRef)
+	const onMouseUp = () =>
+		isTextSelected() || focusInput(input)
+
+	const keyDownListeners: ListenerCollection = {
+		'Enter': () =>
+		{
+			const text = input?.current?.value
+			addPromptToDisplayHistory(text)
+			addValueToInputHistory(text)
+			setInputValue(input)
+			setInputHistoryIndex(-1)
+		},
+		'ArrowUp': () =>
+		{
+			const index = offsetInputHistoryIndex(1)
+			setInputHistoryIndex(index)
+			setInputValue(input, inputHistory[index])
+		},
+		'ArrowDown': () =>
+		{
+			const index = offsetInputHistoryIndex(-1)
+			setInputHistoryIndex(index)
+			setInputValue(input, inputHistory[index])
+		}
+	}
 
 	return (
-		<TerminalWindow onMouseUp={ onTerminalMouseUp }>
+		<TerminalWindow { ...{ onMouseUp } }>
 			{ displayEntries }
 			<PromptLine { ...{
+				input,
 				success,
 				currentDir,
-				inputRef,
-				inputHistory,
-				addToInputHistory,
-				addToDisplayHistory,
+				keyDownListeners
 			} } />
 		</TerminalWindow>
 	)
@@ -49,5 +94,14 @@ export default () =>
 const isTextSelected = () =>
 	window?.getSelection()?.toString()
 
-const focusInputRef = (ref: InputRef) =>
-	ref?.current?.focus()
+const focusInput = (input: InputRef) =>
+	input?.current?.focus()
+
+const setInputValue = (input: InputRef, value: string | undefined = '') =>
+{
+	if (input && input.current)
+	{
+		input.current.value = value || ''
+	}
+}
+
